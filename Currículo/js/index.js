ebe48bd1337b84ee9ae525a3de5acd0791a0ac75
Solange <solidezorzi@gmail.com>
2021-07-22 19:58:08 -0300

    Primeiro commit

---
+++ Currículo/js/index.js
@@ -0,0 +1,34 @@
+var nome = "Solange De Zorzi";
+var cargo="CTO - Grwdev"
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1= document.getElementsById("texto-1");
+var texto2 = document.getElementsById("texto-2");
+
+function colocarNomeNoHtml(nome){
+nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+  cargoHTML.innerHTML = cargo;
+  }
+
+function logarNome() {
+  console.log(nome);
+}
+
+function cliqueNoProjetos(){
+  console.log("clicou no botão projetos");
+  texto2.style.display = "block";
+  texto1.style.display = "none";
+}
+
+function cliqueNoSobre() {
+  console.log("clicou no botão sobre");
+  texto1.style.display = "block";
+  texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtmal(cargo);
